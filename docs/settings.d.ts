36e59003874602cb284aafc9065104c44743b256
Lev Nachmanson <levnach@hotmail.com>
2022-04-13 20:22:39 -0700

    update github.io site
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

---
+++ docs/settings.d.ts
@@ -0,0 +1,8 @@
+export declare const SAMPLE_DOT: string[];
+export declare const ROUTING: {
+    [value: string]: string;
+};
+export declare const LAYOUT: {
+    [value: string]: string;
+};
+export declare const FONT: string[];
