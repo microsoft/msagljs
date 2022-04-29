2e8458c2208be043d6e7d9d2bd400bbfbec5c0e7
Lev Nachmanson <levnach@hotmail.com>
2022-04-29 10:29:00 -0700

    add an example of production graphs

--- examples/layout/src/app.ts
+++ examples/layout/src/app.ts
@@ -5,6 +5,7 @@ import {Renderer, SearchControl, RenderOptions} from '@msagl/renderer'
 import {EdgeRoutingMode, LayerDirectionEnum} from 'msagl-js'
 
 import {SAMPLE_DOT, ROUTING, LAYOUT, FONT} from './settings'
+import {parseDot} from '@msagl/parser'
 
 const DefaultGraph = 'https://raw.githubusercontent.com/microsoft/msagljs/main/examples/data/gameofthrones.json'
 
@@ -12,19 +13,48 @@ const renderer = new Renderer(document.getElementById('viewer'))
 renderer.addControl(new SearchControl())
 
 // Dot file selector
+const queryString = window.location.search
+console.log(queryString)
+const urlParams = new URLSearchParams(queryString)
 const dotFileSelect = <HTMLSelectElement>document.getElementById('gv')
-for (const name of SAMPLE_DOT) {
-  const option = document.createElement('option')
-  option.value = `${name}.gv`
-  option.innerText = name
-  dotFileSelect.appendChild(option)
-}
-dotFileSelect.onchange = () => {
-  const url = 'https://raw.githubusercontent.com/microsoft/msagljs/main/modules/core/test/data/graphvis/' + dotFileSelect.value
-  loadGraphFromUrl(url).then((graph) => {
-    renderer.setGraph(graph)
-    document.getElementById('graph-name').innerText = graph.id
+let url = dotFileSelect.title
+if (urlParams.has('models')) url += '&models=' + urlParams.get('models')
+else url += '&models=ModelY'
+if (urlParams.has('design')) url += '&design=' + urlParams.get('design')
+if (urlParams.has('variants')) url += '&variants=' + urlParams.get('variants')
+console.log(url)
+
+fetch(url)
+  .then((resp) => resp.json())
+  .then((data) => {
+    let first: any
+    for (const k in data) {
+      const option = document.createElement('option')
+      if (!first) {
+        first = k
+      }
+      option.value = k
+      option.text = k
+      option.id = k
+      option.data = data[k]
+      dotFileSelect.appendChild(option)
+      console.log(k)
+    }
+
+    return {id: first, data: data[first]}
   })
+
+  .then((key_val) => {
+    const graph = parseDot(key_val.data)
+    renderer.setGraph(graph)
+    document.getElementById('graph-name').innerText = key_val.id
+  })
+dotFileSelect.onchange = () => {
+  const graphElem = dotFileSelect.options[dotFileSelect.selectedIndex]
+  const dg = graphElem.data
+  const graph = parseDot(dg)
+  renderer.setGraph(graph, getSettings())
+  document.getElementById('graph-name').innerText = graphElem.id
 }
 
 // Settings: edge routing
@@ -73,10 +103,10 @@ dropZone('drop-target', async (f: File) => {
 ;(async () => {
   renderer.setRenderOptions(getSettings())
 
-  const graph = await loadGraphFromUrl(DefaultGraph)
+  // const graph = await loadGraphFromUrl(DefaultGraph)
 
-  renderer.setGraph(graph)
-  document.getElementById('graph-name').innerText = graph.id
+  // renderer.setGraph(graph)
+  // document.getElementById('graph-name').innerText = graph.id
 })()
 
 function getSettings(): RenderOptions {
