b69d3d7d1ab2113c66878d10a54d9d1fd6aa65c3
Lev Nachmanson <levnach@hotmail.com>
2022-05-06 15:07:34 -1000

    work on SVG
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

---
+++ examples/svg/src/app.ts
@@ -0,0 +1,282 @@
+import {parseDot} from '@msagl/parser'
+import {GeomGraph, GeomNode, Graph, ICurve, layoutGraphWithSugiayma, Node, Rectangle} from 'msagl-js'
+import {Color, DrawingGraph, DrawingNode, DrawingObject} from 'msagl-js/drawing'
+import {GeomObject} from '../../../modules/core/src/layout/core/geomObject'
+import {Curve, LineSegment, Point, Polyline} from '../../../modules/core/src/math/geometry'
+import {BezierSeg} from '../../../modules/core/src/math/geometry/bezierSeg'
+import {Ellipse} from '../../../modules/core/src/math/geometry/ellipse'
+import {String} from 'typescript-string-operations'
+const graphString =
+  'digraph abstract {\n' +
+  '	size="6,6";\n' +
+  '  S24 -> 27;\n' +
+  '  S24 -> 25;\n' +
+  '  S1 -> 10;\n' +
+  '  S1 -> 2;\n' +
+  '  S35 -> 36;\n' +
+  '  S35 -> 43;\n' +
+  '  S30 -> 31;\n' +
+  '  S30 -> 33;\n' +
+  '  9 -> 42;\n' +
+  '  9 -> T1;\n' +
+  '  25 -> T1;\n' +
+  '  25 -> 26;\n' +
+  '  27 -> T24;\n' +
+  '  2 -> 3;\n' +
+  '  2 -> 16;\n' +
+  '  2 -> 17;\n' +
+  '  2 -> T1;\n' +
+  '  2 -> 18;\n' +
+  '  10 -> 11;\n' +
+  '  10 -> 14;\n' +
+  '  10 -> T1;\n' +
+  '  10 -> 13;\n' +
+  '  10 -> 12;\n' +
+  '  31 -> T1;\n' +
+  '  31 -> 32;\n' +
+  '  33 -> T30;\n' +
+  '  33 -> 34;\n' +
+  '  42 -> 4;\n' +
+  '  26 -> 4;\n' +
+  '  3 -> 4;\n' +
+  '  16 -> 15;\n' +
+  '  17 -> 19;\n' +
+  '  18 -> 29;\n' +
+  '  11 -> 4;\n' +
+  '  14 -> 15;\n' +
+  '  37 -> 39;\n' +
+  '  37 -> 41;\n' +
+  '  37 -> 38;\n' +
+  '  37 -> 40;\n' +
+  '  13 -> 19;\n' +
+  '  12 -> 29;\n' +
+  '  43 -> 38;\n' +
+  '  43 -> 40;\n' +
+  '  36 -> 19;\n' +
+  '  32 -> 23;\n' +
+  '  34 -> 29;\n' +
+  '  39 -> 15;\n' +
+  '  41 -> 29;\n' +
+  '  38 -> 4;\n' +
+  '  40 -> 19;\n' +
+  '  4 -> 5;\n' +
+  '  19 -> 21;\n' +
+  '  19 -> 20;\n' +
+  '  19 -> 28;\n' +
+  '  5 -> 6;\n' +
+  '  5 -> T35;\n' +
+  '  5 -> 23;\n' +
+  '  21 -> 22;\n' +
+  '  20 -> 15;\n' +
+  '  28 -> 29;\n' +
+  '  6 -> 7;\n' +
+  '  15 -> T1;\n' +
+  '  22 -> 23;\n' +
+  '  22 -> T35;\n' +
+  '  29 -> T30;\n' +
+  '  7 -> T8;\n' +
+  '  23 -> T24;\n' +
+  '  23 -> T1;\n' +
+  '  }\n'
+const graph = parseDot(graphString)
+const dg = <DrawingGraph>DrawingGraph.getDrawingObj(graph)
+dg.createGeometry()
+layoutGraphWithSugiayma(<GeomGraph>GeomGraph.getGeom(graph), null, false)
+
+const svgns = 'http://www.w3.org/2000/svg'
+
+class SvgCreator {
+  svg: any
+  graph: Graph
+  geomGraph: GeomGraph
+  transformRequired: boolean
+  constructor(graph: Graph, transformRequired = false) {
+    this.graph = graph
+    this.transformRequired = transformRequired
+  }
+  createSvg(): any {
+    this.svg = document.createElementNS(svgns, 'svg')
+
+    this.svg.setAttribute('style', 'border: 1px solid black')
+    this.geomGraph = <GeomGraph>GeomGraph.getGeom(this.graph)
+    if (!this.geomGraph) return null
+    this.geomGraph.updateBoundingBox()
+    this.open()
+    for (const node of this.graph.deepNodes) {
+      this.drawNode(node)
+    }
+    this.close()
+    return this.svg
+  }
+  drawNode(node: Node) {
+    const gn = GeomObject.getGeom(node) as GeomNode
+    const boundaryCurve = gn.boundaryCurve
+    if (!boundaryCurve) return
+    this.drawNodeOnCurve(boundaryCurve, node)
+  }
+  drawNodeOnCurve(boundaryCurve: ICurve, node: Node) {
+    const dn = DrawingObject.getDrawingObj(node)
+    const path = document.createElementNS(svgns, 'path')
+    if (dn.fillColor) {
+      path.setAttribute('fill', msaglColorToSvgColor(dn.fillColor))
+    } else {
+      path.setAttribute('fill', 'none')
+    }
+    path.setAttribute('d', curveString(boundaryCurve))
+    path.setAttribute('stroke', msaglColorToSvgColor(dn.color))
+    this.svg.appendChild(path)
+    this.drawLabel(node, dn)
+  }
+  private drawLabel(node: Node, dn: DrawingObject) {
+    if (!dn) return
+    if (!dn.labelText || dn.labelText.length == 0) return
+
+    if (dn instanceof DrawingNode) {
+      this.writeLabelText(node)
+    } else if (dn instanceof DrawingGraph) {
+      throw new Error('not implemented')
+    } else {
+      throw new Error('not implemented')
+    }
+  }
+  private writeLabelText(node: Node) {
+    const geomNode = <GeomNode>GeomNode.getGeom(node)
+    const labelBox = geomNode.boundingBox
+    const x = labelBox.center.x
+    const y = labelBox.center.y
+    const drawingNode = <DrawingNode>DrawingObject.getDrawingObj(node)
+    const fontSize = drawingNode.fontsize
+    const textEl = document.createElementNS(svgns, 'text')
+    textEl.setAttribute('x', x.toString())
+    textEl.setAttribute('y', y.toString())
+    textEl.setAttribute('text-anchor', 'middle')
+    textEl.setAttribute('alignment-baseline', 'middle')
+    textEl.setAttribute('font-family', drawingNode.fontname)
+    textEl.setAttribute('font-size', fontSize.toString())
+
+    textEl.setAttribute('fill', msaglColorToSvgColor(drawingNode.fontColor))
+    textEl.appendChild(document.createTextNode(drawingNode.labelText))
+    this.svg.appendChild(textEl)
+  }
+
+  close() {
+    if (this.transformRequired) {
+      throw new Error('not implemented')
+    }
+  }
+  open() {
+    this.svg.setAttribute('width', this.geomGraph.width)
+    this.svg.setAttribute('height', this.geomGraph.height)
+    if (this.transformRequired) {
+      throw new Error('not implemented')
+    }
+  }
+}
+
+function curveString(iCurve: ICurve): string {
+  return String.Join(' ', Array.from(curveStringTokens(iCurve)))
+}
+
+function* curveStringTokens(iCurve: ICurve): IterableIterator<string> {
+  yield 'M'
+  yield pointToString(iCurve.start)
+  const iscurve = iCurve instanceof Curve
+  if (iscurve) for (const segment of (iCurve as Curve).segs) yield segmentString(segment)
+  else {
+    const islineSeg = iCurve instanceof LineSegment
+    if (islineSeg) {
+      yield 'L'
+      yield pointToString(iCurve.end)
+    } else {
+      const isbezier = iCurve instanceof BezierSeg
+      if (isbezier) {
+        yield this.bezierSegToString(iCurve as BezierSeg)
+      } else {
+        const ispoly = iCurve instanceof Polyline
+        if (ispoly) {
+          const poly = iCurve as Polyline
+          for (const p of poly.skip(1)) {
+            yield 'L'
+            yield pointToString(p.point)
+          }
+          if (poly.closed) {
+            yield 'L'
+            yield pointToString(poly.start)
+          }
+        } else {
+          const isellipse = iCurve instanceof Ellipse
+          if (isellipse) {
+            const ellipse = iCurve as Ellipse
+            if (isFullEllipse(ellipse)) {
+              yield this.ellipseToString(new Ellipse(0, Math.PI, ellipse.aAxis, ellipse.bAxis, ellipse.center))
+              yield this.ellipseToString(new Ellipse(Math.PI, Math.PI * 2, ellipse.aAxis, ellipse.bAxis, ellipse.center))
+            } else yield this.ellipseToString(ellipse)
+          }
+        }
+      }
+    }
+  }
+}
+function isFullEllipse(ell: Ellipse): boolean {
+  return ell.parEnd == Math.PI * 2 && ell.parStart == 0
+}
+
+function pointToString(start: Point) {
+  return doubleToString(start.x) + ' ' + doubleToString(start.y)
+}
+
+function doubleToString(d: number) {
+  return Math.abs(d) < 1e-11 ? '0' : d.toString() //formatForDoubleString, CultureInfo.InvariantCulture);
+}
+
+function bezierSegToString(cubic: BezierSeg): string {
+  return 'C' + pointsToString([cubic.B(1), cubic.B(2), cubic.B(3)])
+}
+
+function ellipseToString(ellipse: Ellipse): string {
+  const largeArc = Math.abs(ellipse.parEnd - ellipse.parStart) >= Math.PI ? '1' : '0'
+  const sweepFlag = ellipse.orientedCounterclockwise() ? '1' : '0'
+
+  return String.Join(
+    ' ',
+    'A',
+    ellipseRadiuses(ellipse),
+    doubleToString(Point.angle(new Point(1, 0), ellipse.aAxis) / (Math.PI / 180.0)),
+    largeArc,
+    sweepFlag,
+    pointToString(ellipse.end),
+  )
+}
+function ellipseRadiuses(ellipse: Ellipse): string {
+  return doubleToString(ellipse.aAxis.length) + ',' + doubleToString(ellipse.bAxis.length)
+}
+function pointsToString(points: Point[]) {
+  return String.Join(
+    ' ',
+    points.map((p) => pointToString(p)),
+  )
+}
+function segmentString(c: ICurve): string {
+  const isls = c instanceof LineSegment
+  if (isls) return lineSegmentString(c as LineSegment)
+
+  const iscubic = c instanceof BezierSeg
+  if (iscubic) return bezierSegToString(c as BezierSeg)
+
+  const isell = c instanceof Ellipse
+  if (isell) return ellipseToString(c as Ellipse)
+
+  throw new Error('NotImplementedException')
+}
+
+function lineSegmentString(ls: LineSegment): string {
+  return 'L ' + pointToString(ls.end)
+}
+
+const svgCreator = new SvgCreator(graph)
+
+document.body.appendChild(svgCreator.createSvg())
+function msaglColorToSvgColor(color: Color): string {
+  if (!color) return 'Black'
+  return 'rgba(' + color.R + ',' + color.G + ',' + color.B + ',' + color.A / 256.0 + ')'
+}
--- modules/parser/src/utils.ts
+++ modules/parser/src/utils.ts
@@ -1,11 +1,11 @@
 import parseCSSColor from 'parse-color'
 import {Color} from 'msagl-js/drawing'
 
-export function parseColor(s: string): Color {
+export function parseColor(s: string, aMult = 255): Color {
   const p = parseCSSColor(s)
   if (p != null) {
     if (p.rgba != null) {
-      return new Color(p.rgba[3] * 255, p.rgba[0], p.rgba[1], p.rgba[2])
+      return new Color(p.rgba[3] * aMult, p.rgba[0], p.rgba[1], p.rgba[2])
     }
     if (p.rgb != null) {
       return Color.mkRGB(p.rgb[0], p.rgb[1], p.rgb[2])
