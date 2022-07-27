39fd5a90b757f9f339a26155b1783bbd21e05450
Lev Nachmanson <levnach@hotmail.com>
2022-07-27 09:40:34 -0700

    port incremental layout
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

--- modules/core/src/utils/cancelToken.ts
+++ modules/core/src/utils/cancelToken.ts
@@ -1,5 +1,8 @@
 // a place holder for the cancelled flag
 export class CancelToken {
+  throwIfCanceled() {
+    throw new Error('Algorithm was cancelled')
+  }
   canceled_: boolean
 
   // Set this flag to true when you want to cancel the layout.
