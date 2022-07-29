aedee27265bcd30d8faccceb47d6d0b2fb9f142a
Lev Nachmanson <levnach@hotmail.com>
2022-07-29 09:19:20 -0700

    work on initial layout
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

--- modules/core/src/layout/core/geomGraph.ts
+++ modules/core/src/layout/core/geomGraph.ts
@@ -8,74 +8,8 @@ import {Point} from '../../math/geometry/point'
 import {OptimalRectanglePacking} from '../../math/geometry/rectanglePacking/OptimalRectanglePacking'
 import {CommonLayoutSettings} from '../layered/commonLayoutSettings'
 import {mkRTree, RTree} from '../../math/geometry/RTree/rTree'
-import {Curve, CurveFactory, ICurve, PointLocation} from '../../math/geometry'
-
-class RRect extends Rectangle {
-  isOk(): boolean {
-    if (this.isEmpty()) {
-      return true
-    }
-    return this.roundedRect_.boundingBox.equalEps(this)
-  }
-  setRect(value: Rectangle) {
-    super.left_ = value.left
-    super.right_ = value.right
-    super.top_ = value.top
-    super.bottom_ = value.bottom
-    if (!this.isEmpty()) {
-      this.roundedRect_ = CurveFactory.mkRectangleWithRoundedCorners(value.width, value.height, this.radX, this.radY, super.center)
-    }
-  }
-  radX: number
-  radY: number
-  roundedRect_: Curve
-  boundingBox_: Rectangle
-  constructor(t: {left: number; right: number; top: number; bottom: number; radX: number; radY: number}) {
-    super(t)
-    this.radX = t.radX
-    this.radY = t.radY
-    this.roundedRect_ = CurveFactory.mkRectangleWithRoundedCorners(super.width, super.height, t.radX, t.radY, super.center)
-  }
-  get center() {
-    return super.center
-  }
-  set center(value: Point) {
-    super.center = value
-    this.roundedRect_ = CurveFactory.mkRectangleWithRoundedCorners(super.width, super.height, this.radX, this.radY, super.center)
-    // Assert.assert(this.isOk())
-  }
-  get left() {
-    return super.left
-  }
-  set left(value: number) {
-    super.left = value
-    this.roundedRect_ = CurveFactory.mkRectangleWithRoundedCorners(super.width, super.height, this.radX, this.radY, super.center)
-  }
-  get right(): number {
-    return super.right
-  }
-  set right(value: number) {
-    super.right = value
-    this.roundedRect_ = CurveFactory.mkRectangleWithRoundedCorners(super.width, super.height, this.radX, this.radY, super.center)
-  }
-  get top() {
-    return super.top
-  }
-
-  set top(value: number) {
-    super.top = value
-    this.roundedRect_ = CurveFactory.mkRectangleWithRoundedCorners(super.width, super.height, this.radX, this.radY, super.center) // todo: optimize
-  }
-  get bottom() {
-    return super.bottom
-  }
-
-  set bottom(value: number) {
-    super.bottom = value
-    if (!this.isEmpty)
-      this.roundedRect_ = CurveFactory.mkRectangleWithRoundedCorners(super.width, super.height, this.radX, this.radY, super.center) // todo: optimize
-  }
-}
+import {Curve, ICurve, PointLocation} from '../../math/geometry'
+import {RRect} from './RRect'
 
 // packs the subgraphs and set the bounding box of the parent graph
 export function optimalPackingRunner(geomGraph: GeomGraph, subGraphs: GeomGraph[]) {
