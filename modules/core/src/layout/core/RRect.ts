56a2b08ca30bc692adc2e42e96a291d7d95011b3
Patrick Wheeler <pawhe@microsoft.com>
2022-09-16 13:43:52 -0700

    Remove property overrides in RRect to work properly when compiled to ES5

--- modules/core/src/layout/core/RRect.ts
+++ modules/core/src/layout/core/RRect.ts
@@ -3,68 +3,39 @@ import {Point} from '../../math/geometry/point'
 import {Curve, CurveFactory} from '../../math/geometry'
 
 export class RRect extends Rectangle {
+  public radX: number
+  public radY: number
+  public roundedRect_: Curve
+  protected boundingBox_: Rectangle
+
+  constructor(t: {left: number; right: number; top: number; bottom: number; radX: number; radY: number}) {
+    super(t)
+
+    this.radX = t.radX
+    this.radY = t.radY
+    this.roundedRect_ = CurveFactory.mkRectangleWithRoundedCorners(this.width, this.height, t.radX, t.radY, this.center)
+  }
+
+  override onUpdated(): void {
+    if (!this.isEmpty) {
+      this.roundedRect_ = CurveFactory.mkRectangleWithRoundedCorners(this.width, this.height, this.radX, this.radY, this.center)
+    }
+  }
+
   isOk(): boolean {
     if (this.isEmpty()) {
       return true
     }
     return this.roundedRect_.boundingBox.equalEps(this)
   }
+
   setRect(value: Rectangle) {
-    super.left_ = value.left
-    super.right_ = value.right
-    super.top_ = value.top
-    super.bottom_ = value.bottom
+    this.left = value.left
+    this.right = value.right
+    this.top = value.top
+    this.bottom = value.bottom
     if (!this.isEmpty()) {
-      this.roundedRect_ = CurveFactory.mkRectangleWithRoundedCorners(value.width, value.height, this.radX, this.radY, super.center)
+      this.roundedRect_ = CurveFactory.mkRectangleWithRoundedCorners(value.width, value.height, this.radX, this.radY, this.center)
     }
-  }
-  radX: number
-  radY: number
-  roundedRect_: Curve
-  boundingBox_: Rectangle
-  constructor(t: {left: number; right: number; top: number; bottom: number; radX: number; radY: number}) {
-    super(t)
-    this.radX = t.radX
-    this.radY = t.radY
-    this.roundedRect_ = CurveFactory.mkRectangleWithRoundedCorners(super.width, super.height, t.radX, t.radY, super.center)
-  }
-  get center() {
-    return super.center
-  }
-  set center(value: Point) {
-    super.center = value
-    this.roundedRect_ = CurveFactory.mkRectangleWithRoundedCorners(super.width, super.height, this.radX, this.radY, super.center)
-    // Assert.assert(this.isOk())
-  }
-  get left() {
-    return super.left
-  }
-  set left(value: number) {
-    super.left = value
-    this.roundedRect_ = CurveFactory.mkRectangleWithRoundedCorners(super.width, super.height, this.radX, this.radY, super.center)
-  }
-  get right(): number {
-    return super.right
-  }
-  set right(value: number) {
-    super.right = value
-    this.roundedRect_ = CurveFactory.mkRectangleWithRoundedCorners(super.width, super.height, this.radX, this.radY, super.center)
-  }
-  get top() {
-    return super.top
-  }
-
-  set top(value: number) {
-    super.top = value
-    this.roundedRect_ = CurveFactory.mkRectangleWithRoundedCorners(super.width, super.height, this.radX, this.radY, super.center) // todo: optimize
-  }
-  get bottom() {
-    return super.bottom
-  }
-
-  set bottom(value: number) {
-    super.bottom = value
-    if (!this.isEmpty)
-      this.roundedRect_ = CurveFactory.mkRectangleWithRoundedCorners(super.width, super.height, this.radX, this.radY, super.center) // todo: optimize
   }
 }
