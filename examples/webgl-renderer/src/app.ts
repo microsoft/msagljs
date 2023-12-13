b0bae64d7cd45d9fdf5885c05c46d8fcb1c7a17c
Lev Nachmanson <levnach@hotmail.com>
2023-12-13 10:31:55 -1000

    fix the webgl exampl

--- examples/webgl-renderer/src/app.ts
+++ examples/webgl-renderer/src/app.ts
@@ -8,9 +8,9 @@ import {SAMPLE_DOT, ROUTING, LAYOUT, FONT} from './settings'
 import {DrawingObject} from '@msagl/drawing'
 import {loadGraphFromFile, loadGraphFromUrl} from '@msagl/parser'
 
+//const defaultGraph = 'https://raw.githubusercontent.com/microsoft/msagljs/main/modules/core/test/data///graphvis/a.gv'
 
-
-const defaultGraph = 'https://raw.githubusercontent.com/microsoft/msagljs/main/examples/data/gameofthrones.json'
+const defaultGraph = 'https://raw.githubusercontent.com/microsoft/msagljs/main/modules/core/test/data/graphvis/p2.gv'
 /// Debug on main thread
 // const renderer = new WebGLRenderer(document.getElementById('viewer'), null)
 /// Test worker with local build
